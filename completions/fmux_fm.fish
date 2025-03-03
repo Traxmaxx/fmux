0db4876b6b24fa31558c2c71ff4f5929859e8df2
Alexander Rösel <320272+Traxmaxx@users.noreply.github.com>
2025-03-03 22:30:24 +0100

    improve autocompletion format and display relevant meta data

--- completions/fmux_fm.fish
+++ completions/fmux_fm.fish
@@ -1,5 +1,22 @@
 # Completions for fm function (tmux session manager)
 
+# Function to list tmux sessions for completion
+function __fmux_list_tmux_sessions
+    command tmux list-sessions -F "#{session_name}" 2>/dev/null
+end
+
+# Function to get session description
+function __fmux_get_session_description
+    set -l session $argv[1]
+    command tmux list-sessions -F "#{session_name}~#{session_windows} • #{t:session_activity} • #{pane_current_command})" 2>/dev/null | grep "^$session~" | cut -d "~" -f 2
+end
+
+# Options
 complete -c fmux_fm -s d -d "Create/attach to session based on directory" -r -a "(__fish_complete_directories)"
 complete -c fmux_fm -s f -d "Find directory and create/attach to session" -f
-complete -c fmux_fm -n "__fish_is_first_arg" -a "(__fmux_list_tmux_sessions)" -f
+
+# Sessions
+for session in (__fmux_list_tmux_sessions)
+    set -l s_description (__fmux_get_session_description "$session")
+    complete -c fmux_fm -n "__fish_is_first_arg" -f -a "$session" -d "$s_description"
+end
--- completions/fmux_fmk.fish
+++ completions/fmux_fmk.fish
@@ -1,6 +1,18 @@
 # Completions for fmk function (tmux session killer)
 
-complete -c fmux_fmk -a "(__fmux_list_tmux_sessions)" -f
+# Function to list tmux sessions for completion
+function __fmux_list_tmux_sessions
+    command tmux list-sessions -F "#{session_name}" 2>/dev/null
+end
 
+# Function to get session description
+function __fmux_get_session_description
+    set -l session $argv[1]
+    command tmux list-sessions -F "#{session_name}~#{session_windows} • #{t:session_activity} • #{pane_current_command})" 2>/dev/null | grep "^$session~" | cut -d "~" -f 2
+end
 
-
+# Sessions
+for session in (__fmux_list_tmux_sessions)
+    set -l s_description (__fmux_get_session_description "$session")
+    complete -c fmux_fmk -n "__fish_is_first_arg" -f -a "$session" -d "$s_description"
+end
